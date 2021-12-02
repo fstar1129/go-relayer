34a0097b79ba395e5fdadf8e48ab3fa4762b69b4
aditya.rout <aditya.rout@latoken.com>
2021-12-02 19:12:44 +0530

    WIP: tested bridge end to end on localnet

--- src/service/workers/eth-compatible/erc20-worker.go
+++ src/service/workers/eth-compatible/erc20-worker.go
@@ -286,19 +286,18 @@ func (w *Erc20Worker) getTransactor() (auth *bind.TransactOpts, err error) {
 	}
 
 	var nonce uint64
-	// if w.chainName == storage.LaChain {
-	// 	nonce, err = w.GetTxCountLatest()
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
+	if w.chainName == storage.LaChain {
+		nonce, err = w.GetTxCountLatest()
+		if err != nil {
+			return nil, err
+		}
 
-	// } else {
-	nonce, err = w.client.PendingNonceAt(context.Background(), w.config.WorkerAddr)
-	if err != nil {
-		return nil, err
+	} else {
+		nonce, err = w.client.PendingNonceAt(context.Background(), w.config.WorkerAddr)
+		if err != nil {
+			return nil, err
+		}
 	}
-	// }
-	// 90000000000,
 
 	auth, err = bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(w.chainID))
 	if err != nil {
