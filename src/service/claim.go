4f1f80fb17dfd0a056b6da67a054a0e3c8d7cbee
aditya.rout <aditya.rout@latoken.com>
2022-02-01 20:22:18 +0530

    to edeploy on testnet

--- src/service/claim.go
+++ src/service/claim.go
@@ -42,6 +42,8 @@ func (r *RelayerSRV) sendClaim(worker workers.IWorker, swap *storage.Swap) (stri
 	}
 
 	var amount int64
+	println(swap.OriginChainID, r.Workers[storage.BSCChain].GetDestinationID())
+	println(swap.ResourceID, r.storage.FetchResourceID("tether").Name)
 	if swap.OriginChainID == r.Workers[storage.BSCChain].GetDestinationID() && swap.ResourceID == r.storage.FetchResourceID("tether").Name {
 		println("in origin")
 		amount = utils.ConvertDecimals(swap.OutAmount, 18, 6)
