3dee3769a124ebd92b96850d9f8eac366115d3c3
aditya.rout <aditya.rout@latoken.com>
2022-03-03 12:13:55 +0530

    removed logging into file

--- src/main.go
+++ src/main.go
@@ -37,12 +37,12 @@ func main() {
 	}
 	logger.SetLevel(level)
 
-	os.MkdirAll("./logs", os.ModePerm)
-	logFile, err := os.OpenFile("./logs/relayer.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-	if err != nil {
-		fmt.Printf("error opening file: %v", err)
-	}
-	logger.SetOutput(logFile)
+	// os.MkdirAll("./logs", os.ModePerm)
+	// logFile, err := os.OpenFile("./logs/relayer.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	// if err != nil {
+	// 	fmt.Printf("error opening file: %v", err)
+	// }
+	// logger.SetOutput(logFile)
 
 	// Set connection to onlife_business database
 	db, err := gorm.Open(dbConfig.DBDriver, dbURL)
